0b800b48a6711e2f7a976da530c5502da720499f
Ken Cheung <ken.cheung@scmp.com>
2018-06-15 11:38:30 +0800

    [build] 0.0.6

--- src/vue-socket.io.ts
+++ src/vue-socket.io.ts
@@ -1,16 +1,27 @@
 import { createDecorator, VueDecorator } from 'vue-class-component'
 
+export interface SocketIoOption {
+  /**
+   * for declaring the socket event name manually
+   */
+  name: string
+}
+
 /**
  * Vue-Socket.io helper
  * Adding the method in the component sockets options
  * https://github.com/MetinSeylan/Vue-Socket.io
  */
-export function SocketIO(): VueDecorator {
+export function SocketIO(socketOption: SocketIoOption): VueDecorator {
   return createDecorator((componentOptions, handler) => {
     const options = componentOptions as any
     if (typeof options.sockets !== 'object') {
       options.sockets = Object.create(null)
     }
-    options.sockets[handler] = options.methods[handler]
+    if (socketOption.name) {
+      options.sockets[socketOption.name] = options.methods[handler]
+    } else {
+      options.sockets[handler] = options.methods[handler]
+    }
   })
 }
